4b0f147eccca7f4a1d056343505de1fec19b6c0c
snahar22 <47393778+snahar22@users.noreply.github.com>
2020-01-27 13:08:10 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
-document.getElementById('getQuote').addEventListener('click', getQuote);
+const quote = document.getElementById('getQuote').addEventListener('click', () => {
+	getQuote();
+});
 
-function getQuote(){
- fetch("https://type.fit/api/quotes")
-.then((response) =>{
-return response.json();
-})
-.then((data) => {
+const getQuote = () => {
+	 fetch("https://type.fit/api/quotes")
+	.then((response) =>{
+	 return response.json();
+	})
+	.then((data) => {
 	let randomise = data[Math.floor(Math.random() * data.length)];
 	document.getElementById('results').innerHTML = randomise.text + "<br>" + randomise.author;
 });
 }
+
+
+
+
+
 
 
 
